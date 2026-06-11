236fe107ec1a8b703e4a20a6257908bfd8b67e44
Ujwala Rekha G <142649169+ujwala123rekha@users.noreply.github.com>
2026-06-11 14:41:57 +0530

    Add Node class for tree representation

--- Tree Representation.java
+++ Tree Representation.java
@@ -1,3 +1,13 @@
+class Node{
+  int data;
+  Node left;
+  Node right;
+  Node(int data){
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
 public class Main{
  public static void main(String[] args){
    Node root = new Node(1);

79ae2c948860bfe3123922f228da5b33322f5cce
Ujwala Rekha G <142649169+ujwala123rekha@users.noreply.github.com>
2026-06-11 18:34:16 +0530

    Create AllDFSTraversals.java

---
+++ AllDFSTraversals.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val) { this.val = val; }
+}
+
+class Pair {
+    TreeNode node;
+    int num;
+    Pair(TreeNode node, int num) {
+        this.node = node;
+        this.num = num;
+    }
+}
+
+public class Solution {
+    public void preInPostTraversal(TreeNode root) {
+        Stack<Pair> st = new Stack<Pair>();
+        st.push(new Pair(root, 1));
+        
+        List<Integer> pre = new ArrayList<>();
+        List<Integer> in = new ArrayList<>();
+        List<Integer> post = new ArrayList<>();
+        
+        if(root == null) return;
+        
+        while(!st.isEmpty()) {
+            Pair it = st.pop();
+            
+            // This is part of pre
+            // increment 1 to 2
+            // push the left side of the tree
+            if(it.num == 1) {
+                pre.add(it.node.val);
+                it.num++;
+                st.push(it);
+                
+                if(it.node.left != null) {
+                    st.push(new Pair(it.node.left, 1));
+                }
+            }
+            // this is a part of in
+            // increment 2 to 3
+            // push right
+            else if(it.num == 2) {
+                in.add(it.node.val);
+                it.num++;
+                st.push(it);
+                
+                if(it.node.right != null) {
+                    st.push(new Pair(it.node.right, 1));
+                }
+            }
+            // don't push it back again
+            else {
+                post.add(it.node.val);
+            }
+        }
+    }
+}

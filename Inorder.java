b1313988df43c07b2ecae0959e4680cfc8b122cb
Ujwala Rekha G <142649169+ujwala123rekha@users.noreply.github.com>
2026-06-11 14:43:12 +0530

    Implement inorder traversal for binary tree

---
+++ Inorder.java
@@ -0,0 +1,25 @@
+class Node{
+  int data;
+  Node left;
+  Node right;
+  Node(int data){
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+public class Main{
+  static void inorder(Node root){
+    if(root == null)return;
+     inorder(root.left);
+     System.out.print(root.data);
+     inorder(root.right);
+  }
+ public static void main(String[] args){
+   Node root = new Node(1);
+   root.left = new Node(2);
+   root.right = new Node(3);
+   root.left.left = new Node(4);
+   inorder(root);
+ }
+}

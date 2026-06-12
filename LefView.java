9bf47ce7d6ffd345afa087d97db3b9e3389243d3
Ujwala Rekha G <142649169+ujwala123rekha@users.noreply.github.com>
2026-06-12 13:27:32 +0530

    Implement left view of a binary tree

---
+++ LefView.java
@@ -0,0 +1,30 @@
+static ArrayList<Integer> leftView(Node root) {
+    ArrayList<Integer> ans = new ArrayList<>();
+
+    if (root == null) return ans;
+
+    Queue<Node> q = new LinkedList<>();
+    q.offer(root);
+
+    while (!q.isEmpty()) {
+
+        int size = q.size();
+
+        for (int i = 0; i < size; i++) {
+
+            Node curr = q.poll();
+
+            if (i == 0) {
+                ans.add(curr.data);
+            }
+
+            if (curr.left != null)
+                q.offer(curr.left);
+
+            if (curr.right != null)
+                q.offer(curr.right);
+        }
+    }
+
+    return ans;
+}
